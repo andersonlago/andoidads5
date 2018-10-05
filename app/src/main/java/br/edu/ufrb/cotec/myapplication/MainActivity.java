7d7be63e2fa644d7750f0c201396e926f028ab0a
anderson <andersonlagogomes@gmail.com>
2018-10-04 21:24:41 -0300

    inicio do projeto

--- app/src/main/java/br/edu/ufrb/cotec/myapplication/MainActivity.java
+++ app/src/main/java/br/edu/ufrb/cotec/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package br.edu.ufrb.cotec.myapplication;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +15,11 @@ import org.springframework.web.client.RestTemplate;
 public class MainActivity extends AppCompatActivity {
 
     TextView nome, email, phone;
+    EditText e;
+    Button b;
+    //objeto para fazer a requisição
+    RestTemplate navegador = new RestTemplate();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +31,8 @@ public class MainActivity extends AppCompatActivity {
         nome = (TextView) findViewById(R.id.Nome);
         email = (TextView) findViewById(R.id.Email);
         phone = (TextView) findViewById(R.id.Phone);
-
-        //objeto para fazer a requisição
-        RestTemplate navegador = new RestTemplate();
+        e = (EditText) findViewById(R.id.editText);
+        b = (Button) findViewById(R.id.button);
 
         //define a classe que fara o tratamento do retorno (Json) para converter em um Objeto Java
         navegador.getMessageConverters().add( new GsonHttpMessageConverter());
@@ -38,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
         nome.setText(r.getResults().get(0).getName().getFirst());
         email.setText(r.getResults().get(0).getEmail());
         phone.setText(r.getResults().get(0).getPhone());
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // OBS: usar http://www.jsonschema2pojo.org para gerar classes pojos do webservice
+                Endereco en = navegador.getForObject("https://viacep.com.br/ws/{p}/json/", Endereco.class,  e.getText().toString()   );
+                Toast.makeText(getApplicationContext(), en.getLocalidade(), Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
 }
